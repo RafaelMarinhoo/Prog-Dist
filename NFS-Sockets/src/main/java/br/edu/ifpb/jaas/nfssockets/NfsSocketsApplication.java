06b92b748df0acbdce2928cf11c12f9971c82f70
Josephadrian123 <Josephadrian123@gmail.com>
2021-08-08 17:55:47 -0300

    Add files via upload

---
+++ NFS-Sockets/src/main/java/br/edu/ifpb/jaas/nfssockets/NfsSocketsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.jaas.nfssockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfsSocketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfsSocketsApplication.class, args);
+    }
+
+}
